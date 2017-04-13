ee93eaf3832d24c8e87a15a5130265ad55012802
Ross Rawlings <Ross.Rawlings01@gmail.com>
2017-04-13 10:02:52 -0700

    Add files via upload

---
+++ makeqr.sh
@@ -0,0 +1,22 @@
+qrencode -o apple3001.png "http://tritechsc.ksd.org/food/index.php?id=3001";
+qrencode -o apple8000.png "http://tritechsc.ksd.org/food/index.php?id=8000";
+qrencode -o apple9000.png "http://tritechsc.ksd.org/food/index.php?id=9000";
+
+qrencode -o bananas4011.png "http://tritechsc.ksd.org/food/index.php?id=4011"
+qrencode -o bananas8011.png "http://tritechsc.ksd.org/food/index.php?id=8011"
+qrencode -o bananas9011.png "http://tritechsc.ksd.org/food/index.php?id=9011"
+
+qrencode -o lettuce4061.png "http://tritechsc.ksd.org/food/index.php?id=4061"
+qrencode -o lettuce8061.png "http://tritechsc.ksd.org/food/index.php?id=8061"
+qrencode -o lettuce9061.png "http://tritechsc.ksd.org/food/index.php?id=9061"
+
+qrencode -o potato4072.png "http://tritechsc.ksd.org/food/index.php?id=4072"
+qrencode -o potato8072.png "http://tritechsc.ksd.org/food/index.php?id=8072"
+qrencode -o potato9072.png "http://tritechsc.ksd.org/food/index.php?id=9072"
+
+qrencode -o tomato4064.png "http://tritechsc.ksd.org/food/index.php?id=4064"
+qrencode -o tomato8064.png "http://tritechsc.ksd.org/food/index.php?id=8064"
+qrencode -o tomato9064.png "http://tritechsc.ksd.org/food/index.php?id=9064"
+
+
+
